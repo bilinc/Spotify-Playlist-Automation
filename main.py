3cc1a75c39aa9c7acb6e77195ec6af8b7d50b6ee
bilinc PC <bilinc@kth.se>
2020-11-21 11:41:14 +0100

    Added gitignore, added spotify api secrets, created pseudo classes for the app

--- main.py
+++ main.py
@@ -5,3 +5,28 @@
 4. Search for the liked songs
 5. Add this song to the new Spotify playlist
 '''
+
+class CreatePlaylist:
+
+	def __init__(self):
+		pass
+
+	# 1. Log into YouTube
+	def get_youtube_client(self):
+		pass
+
+	# 2. Find our liked videos
+	def get_liked_videos(self):
+		pass
+
+	# 3. Create a Spotify playlist
+	def create_playlist(self):
+		pass
+	
+	# 4. Search for the liked songs
+	def get_spotify_url(self):
+		pass
+	# 5. Add like song to the new Spotify playlist
+	def add_song_to_playlist(self):
+		pass
+
